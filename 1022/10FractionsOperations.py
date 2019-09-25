2e351270decfd76843508f1cdfb66412c6c3ff80
Qleoz12 <qleoz12.leonardo@gmail.com>
2019-09-25 00:18:52 -0500

    pruebas de parametros

--- 1022/10FractionsOperations.py
+++ 1022/10FractionsOperations.py
@@ -4,22 +4,21 @@ from mathFractions import mathFractions
 
 my_objects = []
 
-fr=fraction(1,2)
-fr2=fraction(2,4)
-teacher= mathFractions()
-teacher.sumar_fracciones(fr,fr2)
-
-
-'''numeroOperaciones = int(input())
+'captura de numero de iteraciones'
+numeroOperaciones = int(input())
 for i in range(numeroOperaciones):
+   'camptura de cada renglon de par de fracciones'
    operation=input()
+   'se divide cada renglon por espacios'
    data = operation.split()
+   'se arman fracciones'
    fr=fraction(data[0],data[2])
    fr2=fraction(data[4],data[6])
+   'custom Objetc'
+   teacher= mathFractions(data[3],fr,fr2)
+   'add current instance'
+   my_objects.append(teacher)
 
-   
-   my_objects.append(fr)
-   my_objects.append(fr2)
-'''
-
-print(len(my_objects))
+for i in range(numeroOperaciones):
+   my_objects[i].determinate_operation()
+   print(my_objects[i].__dict__)
--- 1022/mathFractions.py
+++ 1022/mathFractions.py
@@ -1,10 +1,41 @@
 from fraction import fraction 
 
 class mathFractions:
-     def __init__(self): 
+     def __init__(self,operation,fraction1,fraction2,num=1,denom=1): 
+          self.operation=operation
+          self.fraction1=fraction1
+          self.fraction2=fraction2
+          self.num=num
+          self.denom=denom
           print("se creo el profe")
 
 
-     def sumar_fracciones(self,fraction1 ,fraction2 ):
-          print("esta mierda suma {} ---".format(fraction1.get_n()))
+     def sumar_fracciones(self,fraction1 ,fraction2):
+           self.num=fraction1.get_n*fraction2.get_d+fraction2.get_n*fraction1.get_d
+           self.denom=fraction1.get_d*fraction2.get_d
+     
+     def restar_fracciones(self,fraction1 ,fraction2 ):
+           self.num=(fraction1.get_n*fraction2.get_d-fraction2.get_n*fraction1.get_d)
+           self.denom=fraction1.get_d*fraction2.get_d
+     
+     def multiplicar_fracciones(self,fraction1 ,fraction2 ):
+           self.num=fraction1.get_n*fraction2.get_n
+           self.denom=fraction1.get_d*fraction2.get_d
+     
+     def dividir_fracciones(self,fraction1 ,fraction2 ):
+          self.num=fraction1.get_n*fraction2.get_d
+          self.denom=fraction2.get_n*fraction1.get_d
 
+
+     def determinate_operation(self):
+          if self.operation == '+':
+               self.sumar_fracciones(self.fraction1,self.fraction2)
+               print("sumo")
+          elif self.operation=="-":
+               self.restar_fracciones(self.fraction1,self.fraction2)
+          elif self.operation=="*":
+               self.multiplicar_fracciones(self.fraction1,self.fraction2)
+          elif self.operation=="/":
+               self.dividir_fracciones(self.fraction1,self.fraction2)
+          else:
+               print("operation no soportada")
