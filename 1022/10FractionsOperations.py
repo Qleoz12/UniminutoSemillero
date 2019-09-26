9ec871bd8895d5f7d1bfdd39775d20de1a2fbd68
Dann <qleoz12.leonardo@gmail.com>
2019-09-26 00:17:50 -0500

    finishing the exercise

--- 1022/10FractionsOperations.py
+++ 1022/10FractionsOperations.py
@@ -12,13 +12,12 @@ for i in range(numeroOperaciones):
    'se divide cada renglon por espacios'
    data = operation.split()
    'se arman fracciones'
-   fr=fraction(data[0],data[2])
-   fr2=fraction(data[4],data[6])
+   fr=fraction(int(data[0]),int(data[2]))
+   fr2=fraction(int(data[4]),int(data[6]))
    'custom Objetc'
    teacher= mathFractions(data[3],fr,fr2)
    'add current instance'
    my_objects.append(teacher)
 
 for i in range(numeroOperaciones):
-   my_objects[i].determinate_operation()
-   print(my_objects[i].__dict__)
+   my_objects[i].result()
--- 1022/mathFractions.py
+++ 1022/mathFractions.py
@@ -7,30 +7,29 @@ class mathFractions:
           self.fraction2=fraction2
           self.num=num
           self.denom=denom
-          print("se creo el profe")
+          'print("se creo el profe")'
 
 
      def sumar_fracciones(self,fraction1 ,fraction2):
-           self.num=fraction1.get_n*fraction2.get_d+fraction2.get_n*fraction1.get_d
-           self.denom=fraction1.get_d*fraction2.get_d
+           self.num=fraction1.get_n()*fraction2.get_d()+fraction2.get_n()*fraction1.get_d()
+           self.denom=fraction1.get_d()*fraction2.get_d()
      
      def restar_fracciones(self,fraction1 ,fraction2 ):
-           self.num=(fraction1.get_n*fraction2.get_d-fraction2.get_n*fraction1.get_d)
-           self.denom=fraction1.get_d*fraction2.get_d
+           self.num=fraction1.get_n()*fraction2.get_d()-fraction2.get_n()*fraction1.get_d()
+           self.denom=fraction1.get_d()*fraction2.get_d()
      
      def multiplicar_fracciones(self,fraction1 ,fraction2 ):
-           self.num=fraction1.get_n*fraction2.get_n
-           self.denom=fraction1.get_d*fraction2.get_d
+           self.num=fraction1.get_n()*fraction2.get_n()
+           self.denom=fraction1.get_d()*fraction2.get_d()
      
      def dividir_fracciones(self,fraction1 ,fraction2 ):
-          self.num=fraction1.get_n*fraction2.get_d
-          self.denom=fraction2.get_n*fraction1.get_d
+          self.num=fraction1.get_n()*fraction2.get_d()
+          self.denom=fraction2.get_n()*fraction1.get_d()
 
 
      def determinate_operation(self):
           if self.operation == '+':
                self.sumar_fracciones(self.fraction1,self.fraction2)
-               print("sumo")
           elif self.operation=="-":
                self.restar_fracciones(self.fraction1,self.fraction2)
           elif self.operation=="*":
@@ -39,3 +38,33 @@ class mathFractions:
                self.dividir_fracciones(self.fraction1,self.fraction2)
           else:
                print("operation no soportada")
+     
+     def simplifyFraction(self):
+          gcd_value=self.determinate_GCD(self.num,self.denom)
+          self.num_simplify=int(self.num/gcd_value)
+          self.denom_simplify=int(self.denom/gcd_value)
+          print("{}/{} = {}/{}".format(self.num,self.denom,self.num_simplify,self.denom_simplify))
+
+     'return the valueof GCD'
+     'uing the  euclidean algorithm, we can determinate de GCD for the fraction n/d'
+     def determinate_GCD(self,numerator,denominator):
+        numerator=abs(numerator)
+        denominator=abs(denominator)
+        if numerator<denominator:
+             pivot=numerator
+             numerator=denominator
+             denominator=pivot
+        if denominator==0:
+             return numerator
+        'first stem into GCD'
+        res=numerator%denominator;
+        while res>0: 
+             numerator=denominator
+             denominator=res
+             res=numerator%denominator
+        
+        return denominator
+
+     def result(self):
+          self.determinate_operation()
+          self.simplifyFraction()
