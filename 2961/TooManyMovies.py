c9adb21f70466495253d5f09dd9d391b05aecbc5
Qleoz12 <qleoz12.leonardo@gmail.com>
2019-11-06 00:14:15 -0500

    2961 complete

--- 2961/TooManyMovies.py
+++ 2961/TooManyMovies.py
@@ -1,11 +1,28 @@
 import sys 
+categoriesNum=4
 inp = sys.stdin
 categories={ 1:0, 2:0, 3:0, 4:0}
 triedyears=int(inp.readline().strip())
-guessedKey="Palpites"
-WinnersKey="Vencedores"
+palpitates=[]
+vencedores=[]
 
 for year in range(triedyears):
-    =inp.readline().strip()
-    
-     
+	inp.readline().strip()
+	for x in range(categoriesNum):
+		palpitates.append(inp.readline().strip())
+	inp.readline().strip()
+	for x in range(categoriesNum):
+		vencedores.append(inp.readline().strip())
+
+for x in range(len(palpitates)):
+	if  palpitates[x]==vencedores[x]:
+		categories.pop((x%4)+1, None)
+
+keys=categories.keys()
+print(*keys, sep=' ', end='\n', file=sys.stdout, flush=False)
+
+	
+		
+
+
+
