9799cab9348a749d604b15499fa26f82822414f7
Dann <qleoz12.leonardo@gmail.com>
2019-11-05 21:51:00 -0500

    new challange 2961

--- 1430/JingleComposing.py
+++ 1430/JingleComposing.py
@@ -1,4 +1,4 @@
-from sys import stdin
+import sys 
 notes_score = {
 	'W': 1.0,
 	'H': 0.5,
@@ -6,15 +6,22 @@ notes_score = {
 	'E': 0.125,
 	'S': 0.0625,
     'T': 0.03125,
-    'X': 0.01562,
+    'X': 0.015625,
 }
-inp = stdin
-jingleLine=inp.readline().strip().split("/")
-counterAmount=0
-counterGeneral=0
-for sublist in jingleLine:
+inp = sys.stdin
+while True:
+    jingleLine=inp.readline().split("/")
     counterAmount=0
-    for value in sublist:
-        counterAmount+=notes_score[value]
-    if counterAmount==1 : counterGeneral+=1  
-print(counterGeneral)
+    counterGeneral=0
+    if len(jingleLine)>2:
+        del jingleLine[-1]
+        del jingleLine[0]
+    for sublist in jingleLine:
+        counterAmount=0
+        for value in sublist:
+            if value in notes_score:
+                counterAmount+=notes_score[value]
+            else:
+                sys.exit()
+        if counterAmount==1 : counterGeneral+=1  
+    print(counterGeneral)
---
+++ 2961/TooManyMovies.py
@@ -0,0 +1,11 @@
+import sys 
+inp = sys.stdin
+categories={ 1:0, 2:0, 3:0, 4:0}
+triedyears=int(inp.readline().strip())
+guessedKey="Palpites"
+WinnersKey="Vencedores"
+
+for year in range(triedyears):
+    =inp.readline().strip()
+    
+     
